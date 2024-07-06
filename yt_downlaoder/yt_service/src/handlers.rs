2da9c4e41fb368192c616023c3a806192c653019
Gunth15 <cjw041@email.latech.edu>
2024-07-05 20:14:06 -0500

    lost a little progress b/c of i deleted the directory smh

---
+++ yt_downlaoder/yt_service/src/handlers.rs
@@ -0,0 +1,6 @@
+use actix_web::{web, HttpResponse};
+
+pub async fn create_video() -> HttpResponse {}
+pub async fn delete_video() -> HttpResponse {}
+pub async fn get_all_videos() -> HttpResponse {}
+pub async fn delete_all_videos() -> HttpResponse {}
---
+++ yt_downlaoder/yt_service/src/lib.rs
@@ -0,0 +1,8 @@
+//redeclrarations
+pub mod dbscript;
+pub mod handlers;
+pub mod ytscripts;
+
+pub mod routes {}
+
+pub mod models {}
