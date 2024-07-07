cb1af2fb20b39dd8568c498547d5392eca93d800
Gunth15 <cjw041@email.latech.edu>
2024-07-07 18:43:25 -0500

    test for later

--- yt_downlaoder/yttest/src/main.rs
+++ yt_downlaoder/yttest/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-    let url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-    let _path_to_video = rustube::blocking::download_best_quality(url).unwrap();
+    let url = "https://youtu.be/nv2wQvn6Wxc";
+    let _path_to_video = rustube::blocking::download_best_quality(url);
 }
